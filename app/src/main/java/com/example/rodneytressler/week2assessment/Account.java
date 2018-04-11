f1296643e5e90c17610ae77b0a241c0d9e451c96
Crystal Adkins <crystaladkins09@gmail.com>
2018-04-11 12:52:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/rodneytressler/week2assessment/Account.java
@@ -0,0 +1,12 @@
+package com.example.rodneytressler.week2assessment;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by rodneytressler on 12/14/17.
+ */
+
+public class Account {
+
+}
